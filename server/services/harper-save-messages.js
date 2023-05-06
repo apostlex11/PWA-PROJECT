d90ffd54fec7491591e7f37c5c76f089f0e474f5
Jack Mabie <johnmabie94@gmail.com>
2023-05-06 14:48:54 -0500

    ran prettier for cleaner formatting

--- server/services/harper-save-messages.js
+++ server/services/harper-save-messages.js
@@ -1,47 +1,47 @@
 // server/services/harper-save-message.js
 
-var axios = require('axios');
+var axios = require("axios");
 
 function harperSaveMessage(message, username, room) {
-const dbUrl = process.env.HARPERDB_URL;
-const dbPw = process.env.HARPERDB_PW;
+  const dbUrl = process.env.HARPERDB_URL;
+  const dbPw = process.env.HARPERDB_PW;
 
-console.log('process.env.HARPERDB_PW :>> ', process.env.HARPERDB_PW);
+  console.log("process.env.HARPERDB_PW :>> ", process.env.HARPERDB_PW);
 
-if (!dbUrl || !dbPw) return null;
+  if (!dbUrl || !dbPw) return null;
 
-var data = JSON.stringify({
-    operation: 'insert',
-    schema: 'realtime_chat_app',
-    table: 'messages',
+  var data = JSON.stringify({
+    operation: "insert",
+    schema: "realtime_chat_app",
+    table: "messages",
     records: [
-    {
+      {
         message,
         username,
         room,
-    },
+      },
     ],
-});
+  });
 
-var config = {
-    method: 'post',
+  var config = {
+    method: "post",
     url: dbUrl,
     headers: {
-    'Content-Type': 'application/json',
-    Authorization: dbPw,
+      "Content-Type": "application/json",
+      Authorization: dbPw,
     },
     data: data,
-};
+  };
 
-return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     axios(config)
-    .then(function (response) {
+      .then(function (response) {
         resolve(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
+      })
+      .catch(function (error) {
         reject(error);
-    });
-});
+      });
+  });
 }
 
 module.exports = harperSaveMessage;
