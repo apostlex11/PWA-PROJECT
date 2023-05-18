2a7ef727e3afe4a823fd2f08709bfa170c9bbe5f
rhubarb414 <ctophr.dubois@gmail.com>
2023-05-17 20:44:17 -0700

    backend works for logging in

--- server/index.js
+++ server/index.js
@@ -123,14 +123,14 @@ app.get('/', (req, res) => {
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
-  await server.start();
-  server.applyMiddleware({ app });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
 
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+        `Use GraphQL at http://localhost:${PORT}${apolloServer.graphqlPath}`
       );
     });
   });
