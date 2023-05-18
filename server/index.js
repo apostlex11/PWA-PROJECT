56b60303a76e3b2e7b6edb8d1b60fdddeccac375
rhubarb414 <ctophr.dubois@gmail.com>
2023-05-17 20:37:36 -0700

    Adds apollo code to index.js

--- server/index.js
+++ server/index.js
@@ -108,3 +108,33 @@ io.on('connection', (socket) => {
 });
 
 server.listen(4000, () => 'Server is running on port 4000');
+
+////set up apollo
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
+// Call the async function to start the server
+startApolloServer();
